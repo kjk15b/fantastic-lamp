e6288385f71a301504e8c49979448c64688ee3ea
Kolby Kiesling <kjk15b@acu.edu>
2022-09-27 15:58:01 -0500

    Forgot init file

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+file_path = os.path.abspath(os.getcwd())+'/todo.db'
+
+app = Flask(__name__)
+print(file_path)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+from app import routes
